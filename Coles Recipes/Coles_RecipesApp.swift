f5703044224394a065d4cb72591c5219afda2111
Alex Guglielmino <alex@bonobolabs.com>
2022-10-19 21:06:47 +0800

    Initial Commit

---
+++ Coles Recipes/Coles_RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coles_RecipesApp.swift
+//  Coles Recipes
+//
+//  Created by Alex Guglielmino on 19/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Coles_RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
