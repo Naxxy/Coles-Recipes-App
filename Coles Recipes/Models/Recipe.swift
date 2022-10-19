d2fe99402b6ba367369aa3d48ebcdea0190c2839
Alex Guglielmino <alex@bonobolabs.com>
2022-10-19 21:27:21 +0800

    Create Recipe model & RecipeDetails model

---
+++ Coles Recipes/Models/Recipe.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe.swift
+//  Coles Recipes
+//
+//  Created by Alex Guglielmino on 19/10/2022.
+//
+
+import Foundation
+
+struct Recipe {
+    let dynamicTitle: String
+    let dynamicDescription: String
+    let dynamicThumbnail: String
+    let dynamicThumbnailAlt: String
+    let recipeDetails: RecipeDetails
+    let ingredients: [String]
+}
---
+++ Coles Recipes/Models/RecipeDetails.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeDetails.swift
+//  Coles Recipes
+//
+//  Created by Alex Guglielmino on 19/10/2022.
+//
+
+import Foundation
+
+struct RecipeDetails {
+    let amountLabel: String
+    let amountNumber: Int
+    let prepLabel: String
+    let prepTime: String
+    let cookingLabel: String
+    let cookingTime: String
+}
