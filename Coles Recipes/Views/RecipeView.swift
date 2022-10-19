0e33a3d411b3ec17599505372058307fe7f2d03a
Alex Guglielmino <alex@bonobolabs.com>
2022-10-19 22:00:26 +0800

    Refactor RecipeView sections into separate views (RecipeDetailsView, IngredientsView)

--- Coles Recipes/Views/RecipeView.swift
+++ Coles Recipes/Views/RecipeView.swift
@@ -30,42 +30,57 @@ struct RecipeView: View {
                     .frame(height: 300)
                 
                 // Recipe Details
-                let recipeDetails = recipe.recipeDetails
-                HStack {
-                    VStack {
-                        Text(recipeDetails.amountLabel)
-                        Text(String(recipeDetails.amountNumber))
-                    }
-                    
-                    VStack {
-                        Text(recipeDetails.prepLabel)
-                        Text(recipeDetails.prepTime)
-                    }
-                    
-                    VStack {
-                        Text(recipeDetails.cookingLabel)
-                        Text(recipeDetails.cookingTime)
-                    }
-                }
+                RecipeDetailsView(recipeDetails: recipe.recipeDetails)
                 
                 // Recipe Ingredients
-                Text("Ingredients")
-                    .font(.largeTitle)
-                    .bold()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.vertical)
-                
-                ForEach(recipe.ingredients, id: \.self) { ingredient in
-                    Label(ingredient, systemImage: "chevron.forward")
-                        .font(.body)
-                }
+                IngredientsView(ingredients: recipe.ingredients)
             }
             .padding()
         }
     }
 }
 
+struct RecipeDetailsView: View {
+    let recipeDetails: RecipeDetails
+    
+    var body: some View {
+        HStack {
+            VStack {
+                Text(recipeDetails.amountLabel)
+                Text(String(recipeDetails.amountNumber))
+            }
+            
+            VStack {
+                Text(recipeDetails.prepLabel)
+                Text(recipeDetails.prepTime)
+            }
+            
+            VStack {
+                Text(recipeDetails.cookingLabel)
+                Text(recipeDetails.cookingTime)
+            }
+        }
+    }
+}
 
+struct IngredientsView: View {
+    let ingredients: [String]
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text("Ingredients")
+                .font(.largeTitle)
+                .bold()
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.vertical)
+            
+            ForEach(ingredients, id: \.self) { ingredient in
+                Label(ingredient, systemImage: "chevron.forward")
+                    .font(.body)
+            }
+        }
+    }
+}
 
 // MARK: - Previews
 struct ContentView_Previews: PreviewProvider {
